438ee7c18eb5f9814ecfa5c75e4ace22175ae241
Oleksii Borysonok <alex.borysonok@gmail.com>
2018-06-12 08:34:08 -0400

    Question 3: Added the dynamic solution.

--- Question_3.js
+++ Question_3.js
@@ -7,18 +7,53 @@
 // 1¢, 3¢
 // 2¢, 2¢
 
-// This is a dynamic programming problem but could also be solved recursively
-function changePossibilities(totalAmount, coinsArray) {
-  if (totalAmount === 0) return 1;
-  if (totalAmount < 0) return 0;
-  if (coinsArray.length <= 0 && totalAmount >= 1) return 0;
+// ========================================================================================== //
 
-  return (
-    changePossibilities(totalAmount, coinsArray.slice(0, -1)) +
-    changePossibilities(totalAmount - coinsArray[coinsArray.length - 1], coinsArray)
-  );
+// This is a dynamic programming problem but could also be solved recursively:
+
+function changePossibilities(totalAmount, coinsArray) {
+    if (totalAmount === 0) return 1;
+    if (totalAmount < 0) return 0;
+    if (coinsArray.length <= 0 && totalAmount >= 1) return 0;
+
+    return (
+        changePossibilities(totalAmount, coinsArray.slice(0, -1)) +
+        changePossibilities(totalAmount - coinsArray[coinsArray.length - 1], coinsArray)
+    );
 }
 
 console.log(changePossibilities(4, [1, 2, 3])); // 4
 console.log(changePossibilities(5, [1, 2, 3])); // 5
 console.log(changePossibilities(6, [1, 2, 3])); // 7
+
+
+// ========================================================================================== //
+
+// I have also found the solusion that utilizes Dynamic Programming way:
+
+function changePossibilities(totalAmount, coinsArray) {
+    let waysForTotalAmount = [];
+    for (let i = 0; i <= totalAmount; i++) {
+        waysForTotalAmount[i] = 0;
+    }
+    // there is 1 way to renturn 0 cents
+    waysForTotalAmount[0] = 1;
+
+    for (let j = 0; j < coinsArray.length; j++) {
+        let coin = coinsArray[j];
+        for (let higherAmount = coin; higherAmount <= totalAmount; higherAmount++) {
+            let higherAmountRemainder = higherAmount - coin;
+            waysForTotalAmount[higherAmount] += waysForTotalAmount[higherAmountRemainder];
+        }
+    }
+
+    return waysForTotalAmount[totalAmount];
+}
+
+
+
+console.log(changePossibilities(4, [1, 2, 3])); // 4
+console.log(changePossibilities(5, [1, 2, 3])); // 5
+console.log(changePossibilities(6, [1, 2, 3])); // 7
+
+// ========================================================================================== //
