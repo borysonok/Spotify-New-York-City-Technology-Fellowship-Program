cad7dc5449a5e6189d76197e1e0edfde6d1be1fb
Oleksii Borysonok <alex.borysonok@gmail.com>
2018-06-11 21:08:00 -0400

    Question 3 solved | refactored with recursion

--- Question_3.js
+++ Question_3.js
@@ -1,42 +1,24 @@
-// Write a function that, given an amount of money and an array of coin denominations, computes the number of ways to make the amount of money with coins of the available denominations. 
+// Write a function that, given an amount of money and an array of coin denominations, computes the number of ways to make the amount of money with coins of the available denominations.
 
-// Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would output 4—the number of ways to make 4¢ with those denominations: 
+// Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would output 4—the number of ways to make 4¢ with those denominations:
 
 // 1¢, 1¢, 1¢, 1¢
 // 1¢, 1¢, 2¢
 // 1¢, 3¢
 // 2¢, 2¢
 
-
+// This is a dynamic programming problem but could also be solved recursively
 function changePossibilities(totalAmount, coinsArray) {
-    let numberOfWays = 0;
-    let allWaysArr = [];
+  if (totalAmount === 0) return 1;
+  if (totalAmount < 0) return 0;
+  if (coinsArray.length <= 0 && totalAmount >= 1) return 0;
 
-    for (let i = 0; i < coinsArray.length; i++) {
-        let tempWays = [];
-        let tempSum = coinsArray[i];
-        tempWays.push(coinsArray[i]);
-
-        for (let j = 0; j < coinsArray.length; j++) {
-            while (tempSum < totalAmount) {
-                tempSum += coinsArray[j];
-                tempWays.push(coinsArray[j]);
-                if (tempSum === totalAmount) {
-                    numberOfWays++;
-                    allWaysArr.push(tempWays);
-                    break;
-                }
-            }
-        }
-        tempWays = [];
-        tempSum = coinsArray[i];
-    }
-    console.log("Ways: ", allWaysArr);
-
-    return numberOfWays;
+  return (
+    changePossibilities(totalAmount, coinsArray.slice(0, -1)) +
+    changePossibilities(totalAmount - coinsArray[coinsArray.length - 1], coinsArray)
+  );
 }
 
-
-console.log(changePossibilities(4, [1, 2, 3]));
-console.log(changePossibilities(5, [1, 2, 3]));
-console.log(changePossibilities(6, [1, 2, 3]));
+console.log(changePossibilities(4, [1, 2, 3])); // 4
+console.log(changePossibilities(5, [1, 2, 3])); // 5
+console.log(changePossibilities(6, [1, 2, 3])); // 7
